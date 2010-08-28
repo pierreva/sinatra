465fbc46ef24ff279d409b9342f74578c2fd2f40
pierreva <pierre.vaudrey@gmail.com>
2010-08-28 14:38:01 +0200

    first commit

---
+++ beta.rb
@@ -0,0 +1,8 @@
+require 'rubygems'
+require 'sinatra'
+get '/' do
+erb :index
+end
+post '/submit' do
+erb :submit
+end
---
+++ views/layout.rb
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Beta Info</title>
+</head>
+<body>
+<%= yield %>
+</body>
+</html>
